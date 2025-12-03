29d44ff79c1a3dc6c4de25adfa8583663b24bfad
paolo <pao.sarno@gmail.com>
2025-12-03 11:44:26 +0100

    initialisation projet

--- Base-c#/Base-c#/obj/Debug/net8.0/Base-c#.AssemblyInfo.cs
+++ Base-c#/Base-c#/obj/Debug/net8.0/Base-c#.AssemblyInfo.cs
@@ -11,15 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("basesCsharp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Base-c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("basesCsharp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("basesCsharp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Base-c#")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Base-c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
